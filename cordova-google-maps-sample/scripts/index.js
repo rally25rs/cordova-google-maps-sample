3f83ca4da615f7ca3b0c0db5a2fe86f737016241
Jeff Valore <rally25rs@yahoo.com>
2014-10-28 20:47:51 -0400

    Initial Commit

---
+++ cordova-google-maps-sample/scripts/index.js
@@ -0,0 +1,36 @@
+﻿(function (global) {
+    "use strict";
+
+    function onDeviceReady() {
+        document.addEventListener("online", onOnline, false);
+        document.addEventListener("resume", onResume, false);
+        loadMapsApi();
+    }
+
+    function onOnline() {
+        loadMapsApi();
+    }
+
+    function onResume() {
+        loadMapsApi();
+    }
+
+    function loadMapsApi() {
+        if (navigator.connection.type === Connection.NONE || (global.google !== undefined && global.google.maps)) {
+            return;
+        }
+
+        //TODO: Add your own Google maps API key to the URL below.
+        $.getScript('https://maps.googleapis.com/maps/api/js?sensor=true&callback=onMapsApiLoaded');
+    }
+
+    global.onMapsApiLoaded = function () {
+        // Maps API loaded and ready to be used.
+        var map = new google.maps.Map(document.getElementById("map"), {
+            zoom: 8,
+            center: new google.maps.LatLng(-34.397, 150.644)
+        });
+    };
+
+    document.addEventListener("deviceready", onDeviceReady, false);
+})(window);
